2c2f10bbd5badbde48191178c84cacfb0c0854b6
daffa017 <daffazakwan26@gmail.com>
2023-03-27 08:08:21 +0700

    Commit 1

---
+++ app/src/main/java/com/example/tugasakhir_foodapp_kelompok3/Activity_tiga.kt
@@ -0,0 +1,11 @@
+package com.example.tugasakhir_foodapp_kelompok3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_tiga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tiga)
+    }
+}
